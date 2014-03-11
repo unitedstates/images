d2c3be3fa6793b518095721e5461fd378c0a486f
hugovk <hugovk@users.noreply.github.com>
2014-03-11 14:36:51 +0200

    Helper script to list missing images. Run like: scripts/missing.py

---
+++ scripts/missing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Find missing images.
+"""
+import os
+import gpo_member_photos
+
+if __name__ == "__main__":
+    # clone or update legislator YAML
+    gpo_member_photos.download_legislator_data()
+
+    legislators = gpo_member_photos.load_yaml("congress-legislators/legislators-current.yaml")
+    for l in legislators:
+        bioguide = l['id']['bioguide']
+        filename = os.path.join("congress", "original", bioguide + ".jpg")
+        if not os.path.exists(filename):
+            print "---"
+            print "Not found:", filename
+            print l['name']
+
+# End of file
